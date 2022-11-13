b1bf7e88e51591a46498015ca3628b04c1ed5035
Himanshu Agrahari <57482480+HimanshuAgrahari07@users.noreply.github.com>
2022-11-13 23:59:20 +0530

    Revert "Add basic template for database and query helper"

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()../database/table.post
+@Injectable()
 export class PostService {
   async findAll() {
     return `This action returns all post`;
--- src/utils/types/index.ts
+++
@@ -1,5 +0,0 @@
-export type UnionToIntersection<U> = (
-  U extends never ? never : (arg: U) => never
-) extends (arg: infer I) => void
-  ? I
-  : never;
